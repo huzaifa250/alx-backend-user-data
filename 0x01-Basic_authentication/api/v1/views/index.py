5252832eb256e3b3795b35163e8c07d7b92eb128
huzaifa250 <huz.dark1@gmail.com>
2024-11-13 09:14:01 +0000

    test authorization

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-""" Module of Index views
-"""
+""" Module of Index views"""
 from flask import jsonify, abort
 from api.v1.views import app_views
 
@@ -25,3 +24,10 @@ def stats() -> str:
     stats['users'] = User.count()
     return jsonify(stats)
 
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> str:
+    """ GET /api/v1/unauthorized 'task 1'
+    Return:
+        - raise a 401 error
+    """
+    abort(401, description="Unauthorized")
