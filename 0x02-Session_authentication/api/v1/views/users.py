bd3594b10a175123a0278575136cc29b6e95871e
huzaifa250 <huz.dark1@gmail.com>
2024-11-14 10:44:53 +0000

    task 0,1

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -28,6 +28,12 @@ def view_one_user(user_id: str = None) -> str:
     if user_id is None:
         abort(404)
     user = User.get(user_id)
+    if user_id == 'me':
+        if request.current_user is None:
+            abort(404)
+        user = request.current_user
+        return jsonify(user.to_json())
+    user = User.get(user_id)
     if user is None:
         abort(404)
     return jsonify(user.to_json())
@@ -120,4 +126,3 @@ def update_user(user_id: str = None) -> str:
         user.last_name = rj.get('last_name')
     user.save()
     return jsonify(user.to_json()), 200
-
